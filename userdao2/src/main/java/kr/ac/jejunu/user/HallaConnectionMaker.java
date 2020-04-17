eff6590873999e6bf7a98385818f0c882e4b66d6
DongHunWon <37091640+DongHunWon@users.noreply.github.com>
2020-04-17 18:35:41 +0900

    04.17 3

--- userdao2/src/main/java/kr/ac/jejunu/user/HallaConnectionMaker.java
+++ userdao2/src/main/java/kr/ac/jejunu/user/HallaConnectionMaker.java
@@ -4,10 +4,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class JejuUserDao extends UserDao {
-
+public class HallaConnectionMaker implements ConnectionMaker {
     @Override
-    Connection getConnection() throws ClassNotFoundException, SQLException {
+    public Connection getConnection() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
         //connection
         return DriverManager.getConnection("jdbc:mysql://localhost/jeju?serverTimezone=Asia/Seoul", "jeju", "jejupw");
--- userdao2/src/main/java/kr/ac/jejunu/user/JejuConnectionMaker.java
+++ userdao2/src/main/java/kr/ac/jejunu/user/JejuConnectionMaker.java
@@ -4,9 +4,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class HallaUserDao extends UserDao {
+public class JejuConnectionMaker implements ConnectionMaker {
     @Override
-    Connection getConnection() throws ClassNotFoundException, SQLException {
+    public Connection getConnection() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
         //connection
         return DriverManager.getConnection("jdbc:mysql://localhost/jeju?serverTimezone=Asia/Seoul", "jeju", "jejupw");
